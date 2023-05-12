ab03a98fbbc86cfc04beb4d55917e65a4a278e8c
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-12 16:22:49 +0300

    2 ex

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery")
+
+galleryBox.insertAdjacentHTML("afterbegin", createImg(galleryItems));
+
+function createImg(galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`})
+    .join("")
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+
+
+
+// galleryContainer.innerHTML = galleryItems.reduce(
+//     (html, el) =>
+//         html +
+//         `<li>
+//             <a class="gallery__item" href="${el.original}">
+//                 <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+//             </a>
+//         </li>`,
+//     ''
+// );
+
