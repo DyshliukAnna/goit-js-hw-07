8aa4e60b88c0f55624d39267f8fab675d891a03e
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-12 14:05:42 +0300

    change all

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,37 +5,31 @@ console.log(galleryItems);
 
 const galleryBox = document.querySelector(".gallery");
 
-galleryBox.insertAdjacentHTML(
-  "beforeend",
-  createImgGalleryMarkup(galleryItems)
-);
-galleryBox.addEventListener("click", onImgGalleryItemClick);
 
-function createImgGalleryMarkup(images) {
-  return images
-    .map(({ original, preview, description }) => {
-      return `<div class="gallery__item">
-		<a class="gallery__link" href="${original}">
-		  <img
-			class="gallery__image"
-			src="${preview}	"
-			data-source="${original}"
-			alt="${description}"
-		  />
-		</a>
-	  </div>`;
-    })
-    .join("");
-}
+galleryBox.innerHTML = galleryItems.map(({ preview, original, description }) => {
+  return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+})
+  .join("")
 
-function onImgGalleryItemClick(event) {
+galleryBox.addEventListener("click", onClick);
+
+function onClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
-    return;
+    return
   }
   const urlOriginal = event.target.dataset.source;
-
-  //Створеня екземпляра бібліотеки basicLightbox
+  // Створеня екземпляра бібліотеки basicLightbox
   // по кліку на картинку відкривається картинка оригільного розміру в окремому модальному вікні
   const instance = basicLightbox.create(
     `
@@ -52,7 +46,6 @@ function onImgGalleryItemClick(event) {
     }
   );
   instance.show();
-
   function onEscape(event) {
     console.log(event.code);
     if (event.code === "Escape") {
@@ -62,3 +55,83 @@ function onImgGalleryItemClick(event) {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// // console.log(galleryItems);
+
+// const galleryContainer = document.querySelector('.gallery');
+
+// galleryContainer.innerHTML = galleryItems.reduce(
+//     (html, el) =>
+//         html +
+//         `<div class="gallery__item">
+//         <a class="gallery__link" href="${el.original}">
+//             <img
+//             class="gallery__image"
+//             src="${el.preview}"
+//             data-source="${el.original}"
+//             alt="Image description"
+//             />
+//         </a>
+//         </div>`,
+//     ''
+// );
+
+// const onImageClick = e => {
+//     if (e.target.nodeName !== 'IMG') {
+//         return;
+//     }
+//     e.preventDefault();
+
+//     const onEscape = e => {
+//         const keyCode = 'Escape';
+//         // console.log(e.code);
+//         if (e.code === keyCode) {
+//             instance.close();
+//         }
+//     };
+
+//     const instance = basicLightbox.create(
+//         `<img class="gallery__image"
+//             src="${e.target.dataset.source}"
+//             alt="Image description"
+//             />`,
+//         {
+//             onShow: () => window.addEventListener('keydown', onEscape),
+//             onClose: () => window.removeEventListener('keydown', onEscape)
+//         }
+//     );
+//     instance.show();
+// };
+
+// galleryContainer.addEventListener('click', onImageClick);
